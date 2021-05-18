fd2538e9706be7975c2b6b3be0ffb9cda71b6eee
EricBL3 <ericbuitron3@gmail.com>
2021-05-18 10:34:17 -0500

    Add GPU Instancing

---
+++ GPU Flocking/Assets/Scripts/FlockGPU.hlsl
@@ -0,0 +1,23 @@
+﻿#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+	struct FlockUnit
+	{
+		float3 position;
+		float4 rotation;
+		float speed;
+	};
+	StructuredBuffer<FlockUnit> flockUnitBuffer;
+#endif
+
+void ConfigureProcedural() {
+#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+	float3 position = flockUnitBuffer[unity_InstanceID].position;
+
+	unity_ObjectToWorld = 0.0;
+	unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
+	unity_ObjectToWorld._m00_m11_m22 = 1.0;
+#endif
+}
+
+void ShaderFunction_float(float3 In, out float3 Out) {
+	Out = In;
+}
---
+++ GPU Flocking/Assets/Scripts/GPU_Flock.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+
+public class GPU_Flock : MonoBehaviour
+{
+    private struct FlockUnit
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public float speed;
+    }
+
+    //one float, one vector3 and one vector4 with float values
+    private const int FLOCK_UNIT_SIZE = (1 + 3 + 4) * sizeof(float);
+
+    private const int THREAD_GROUPS = 256;
+
+    [Header("Spawn Setup")]
+    [SerializeField]
+    private Mesh flockUnityMesh;
+    [SerializeField]
+    private int submeshIndex = 0;
+    [SerializeField]
+    private Material flockUnityMaterial;
+    [SerializeField]
+    private ComputeShader flockComputeShader;
+    private int computeKernelIndex;
+    [SerializeField]
+    private int flockSize = 100;
+    [SerializeField]
+    private Vector3 spawnBounds;
+
+    [Header("Flock Unit Setup")]
+    [SerializeField]
+    private float FOVAngle;
+    [SerializeField]
+    private float smoothDamp;
+    [SerializeField]
+    private LayerMask obstacleMask;
+    [SerializeField]
+    private Vector3[] directionsToCheckWhenAvoidingObstacles;
+
+    [Header("Speed Setup")]
+    [SerializeField, Range(0, 10)]
+    private float _minSpeed;
+    public float minSpeed { get { return _minSpeed; } }
+    [SerializeField, Range(0, 10)]
+    private float _maxSpeed;
+    public float maxSpeed { get { return _maxSpeed; } }
+
+    [Header("Detection Distance")]
+    [SerializeField, Range(0, 10)]
+    private float _cohesionDistance;
+    public float cohesionDistance { get { return _cohesionDistance; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _avoidanceDistance;
+    public float avoidanceDistance { get { return _avoidanceDistance; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _alignmentDistance;
+    public float alignmentDistance { get { return _alignmentDistance; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _obstacleDistance;
+    public float obstacleDistance { get { return _obstacleDistance; } }
+
+    [SerializeField, Range(0, 100)]
+    private float _boundsDistance;
+    public float boundsDistance { get { return _boundsDistance; } }
+
+    [Header("Behavior Weight")]
+    [SerializeField, Range(0, 10)]
+    private float _cohesionWeight;
+    public float cohesionWeight { get { return _cohesionWeight; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _avoidanceWeight;
+    public float avoidanceWeight { get { return _avoidanceWeight; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _alignmentWeight;
+    public float alignmentWeight { get { return _alignmentWeight; } }
+
+    
+    [SerializeField, Range(0, 10)]
+    private float _boundsWeight;
+    public float boundsWeight { get { return _boundsWeight; } }
+
+    [SerializeField, Range(0, 100)]
+    private float _obstacleWeight;
+    public float obstacleWeight { get { return _obstacleWeight; } }
+
+    private ComputeBuffer flockUnitBuffer;
+
+    private void Start()
+    {
+        if (flockSize < 1)
+            flockSize = 1;
+
+        //FLOCK UNITY ARRAY AND BUFFER
+        FlockUnit[] flockUnits = new FlockUnit[flockSize];
+        for(int i = 0; i < flockSize; i++)
+        {
+            var randomVector = UnityEngine.Random.insideUnitSphere;
+            randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y, randomVector.z * spawnBounds.z);
+            var spawnPosition = transform.position + randomVector;
+            var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+            flockUnits[i].position = spawnPosition;
+            flockUnits[i].rotation = rotation;
+            flockUnits[i].speed = UnityEngine.Random.Range(minSpeed, maxSpeed);
+        }
+        flockUnitBuffer = new ComputeBuffer(flockSize, FLOCK_UNIT_SIZE);
+        flockUnitBuffer.SetData(flockUnits);
+
+        //SEND DATA TO COMPUTE SHADER
+        computeKernelIndex = flockComputeShader.FindKernel("CSMain");
+        flockComputeShader.SetBuffer(computeKernelIndex, "flockUnitBuffer", flockUnitBuffer);
+        flockUnityMaterial.SetBuffer("flockUnitBuffer", flockUnitBuffer);
+    }
+
+    private void Update()
+    {
+        //dispatch kernel
+        int groups = Mathf.CeilToInt(flockSize / THREAD_GROUPS);
+        //flockComputeShader.Dispatch(computeKernelIndex, groups, 1, 1);
+        //render
+        flockUnityMaterial.SetPass(0);
+        Graphics.DrawMeshInstancedProcedural(flockUnityMesh, submeshIndex, flockUnityMaterial, 
+            new Bounds(transform.position, spawnBounds), flockSize);
+    }
+
+    void OnDestroy()
+    {
+        if (flockUnitBuffer != null)
+        {
+            flockUnitBuffer.Release();
+            flockUnitBuffer = null;
+        }
+    }
+
+}
